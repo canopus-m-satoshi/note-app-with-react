aa33aef9147101e1480de23a4efd2926ab5215f9
Satoshi Matsushima <work.satoshi1993@gmail.com>
2023-02-23 18:50:24 +0900

    updated  Sidebar.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import { useState } from 'react'
 import Main from './components/Main'
 import Sidebar from './components/Sidebar'
-
-type Notes = {
-  id: number
-  title: string
-  content: string
-  modDate: number
-}
+import { Notes } from './types'
 
 function App() {
-  const [notes, setNotes] = useState([])
+  const [notes, setNotes] = useState<Array<Notes>>([])
 
   const onAddNote = () => {
     const newNote: Notes = {
-      id: 1,
+      id: Date.now(),
       title: 'test',
       content:
         'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, quam pariatur? Ullam maiores quisquam consequatur sunt dolorem dignissimos sint. Vero saepe cupiditate eius autem repellat sunt quam eaque officiis itaque.',
@@ -26,7 +20,7 @@ function App() {
 
   return (
     <div className="App bg-blue-50 w-full h-screen overflow-hidden flex">
-      <Sidebar onAddNote={onAddNote} />
+      <Sidebar onAddNote={onAddNote} notes={notes} />
       <Main />
     </div>
   )
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,11 @@
+import { Notes } from '../types'
+
 type Props = {
   onAddNote: () => void
+  notes: Array<Notes>
 }
 
-const Sidebar: React.FC<Props> = ({ onAddNote }) => {
+const Sidebar: React.FC<Props> = ({ onAddNote, notes }) => {
   return (
     <div className="border-r  h-full w-44 py-2">
       <div className="flex items-center justify-between p-2">
@@ -11,15 +14,26 @@ const Sidebar: React.FC<Props> = ({ onAddNote }) => {
           Add
         </button>
       </div>
+
       <div className="h-[calc(100vw+78px)] overflow-y-scroll mt-2 pl-2">
-        <div className="hover:opacity-40 duration-300 cursor-pointer">
-          <div className="flex items-center justify-between">
-            <strong>Title</strong>
-            <button className="btn btn-active btn-ghost">Delete</button>
+        {notes.map((note) => (
+          <div
+            key={note.id}
+            className="hover:opacity-40 duration-300 cursor-pointer">
+            <div className="flex items-center justify-between">
+              <strong>{note.title}</strong>
+              <button className="btn btn-active btn-ghost">Delete</button>
+            </div>
+            <p className="my-2">{note.content}</p>
+            <small className="text-gray-500">
+              Last update:{' '}
+              {new Date(note.modDate).toLocaleDateString('ja-JP', {
+                hour: '2-digit',
+                minute: '2-digit',
+              })}
+            </small>
           </div>
-          <p className="my-2">Contents</p>
-          <small className="text-gray-500">Last update: xx/xx</small>
-        </div>
+        ))}
       </div>
     </div>
   )
